5ac21cea10b0b250e42c15ac0a8a8bbd81473f2d
Raoul Wols <raoulwols@gmail.com>
2016-11-14 11:51:36 +0100

    Added new application command 'open_clara_settings'.

--- GenerateSystemHeadersCommand.py
+++ GenerateSystemHeadersCommand.py
@@ -1,19 +1,26 @@
 import sublime, sublime_plugin, subprocess, os
 
-SHELL_CMD = "clang++ -E -x c++ - -v < /dev/null 2>&1 | awk '/#include <...> search starts here/{f=1;next} /End of search list./{f=0} f'"
+SHELL_CMD1 = "clang++ -E -x c++ - -v < /dev/null 2>&1 | awk '/#include <...> search starts here/{f=1;next} /End of search list./{f=0} f'"
+SHELL_CMD2 = "clang++ -E -x c++ - -v < /dev/null 2>&1 | awk 'BEGIN{FS=\"-resource-dir\"} /-resource-dir/ {print $2}' | awk '{print $1;}'"
+USER_MSG = 'System headers have been saved in user settings. Do you want to view them now?'
+SETTINGS = 'Clara.sublime-settings'
 
 class GenerateSystemHeadersCommand(sublime_plugin.ApplicationCommand):
 	"""Generates system header info."""
 
 	def run(self):
-		output = subprocess.check_output(SHELL_CMD, shell=True)
+		output = subprocess.check_output(SHELL_CMD1, shell=True)
 		output = output.decode('utf-8')
 		headers = output.splitlines()
 		headers = [os.path.abspath(x.strip()) for x in headers]
-		settings = sublime.load_settings('Clara')
+		output = subprocess.check_output(SHELL_CMD2, shell= True)
+		builtinHeaders = os.path.abspath(output.decode('utf-8').strip())
+		settings = sublime.load_settings(SETTINGS)
 		settings.set('system_headers', headers)
-		sublime.save_settings('Clara')
-		sublime.message_dialog('System headers have been saved in user settings.')
+		settings.set('builtin_headers', builtinHeaders)
+		sublime.save_settings(SETTINGS)
+		if sublime.ok_cancel_dialog(USER_MSG):
+			sublime.run_command('open_clara_settings')
 
 	def description(self):
 		return 'Generates system headers.'
