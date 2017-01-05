14cb0c83bd3f10985c970d8b8cc33ffe1c81f3fc
Raoul Wols <raoulwols@gmail.com>
2017-01-05 20:35:57 +0100

    Fixed a bug where the correct python version was not found.
    After pulling in new changes from pybind11. Also removed
    the "code complete at cursor" command, since Sublime has something
    like that built-in.

--- plugin/CodeCompleteAtCursorCommand.py
+++
@@ -1,7 +0,0 @@
-import sublime, sublime_plugin
-
-class CodeCompleteAtCursorCommand(sublime_plugin.TextCommand):
-
-	def run(self, edit):
-		self.view.run_command('hide_auto_complete')
-		self.view.run_command('auto_complete')
