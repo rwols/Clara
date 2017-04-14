5091fd75f7cdb64f24543714c2720b2416b98d94
Raoul Wols <raoulwols@gmail.com>
2017-04-14 10:57:44 +0200

    Minor clean ups and tweaks

--- plugin/commands/write_system_headers.py
+++ plugin/commands/write_system_headers.py
@@ -28,9 +28,6 @@ class ClaraWriteSystemHeadersCommand(sublime_plugin.ApplicationCommand):
         
         SHELL_CMD1 = """clang++ -E -x c++ - -v < /dev/null 2>&1 | awk '/#include <...> search starts here/{f=1;next} /End of search list./{f=0} f'"""
         SHELL_CMD2 = """clang++ -E -x c++ - -v < /dev/null 2>&1 | awk 'BEGIN{FS="-resource-dir"} /-resource-dir/ {print $2}' | awk '{print $1;}'"""
-        USER_MSG = 'System headers have been saved in user settings. Do you want to view them now?'
-        SETTINGS = 'Clara.sublime-settings'
-        ENDS_WITH_FRAMEWORK = ' (framework directory)'
 
         output = self.subprocess(SHELL_CMD1)
         raw_headers = output.splitlines()
@@ -38,8 +35,8 @@ class ClaraWriteSystemHeadersCommand(sublime_plugin.ApplicationCommand):
         headers = []
         for raw_header in raw_headers:
             raw_header = raw_header.strip()
-            if raw_header.endswith(ENDS_WITH_FRAMEWORK):
-                raw_header = raw_header[:-len(ENDS_WITH_FRAMEWORK)]
+            if raw_header.endswith(' (framework directory)'):
+                raw_header = raw_header[:-len(' (framework directory)')]
                 frameworks.append(os.path.abspath(raw_header))
             else:
                 headers.append(os.path.abspath(raw_header))
@@ -47,7 +44,7 @@ class ClaraWriteSystemHeadersCommand(sublime_plugin.ApplicationCommand):
         resource_dir = os.path.abspath(output)
         builtin_include = os.path.join(resource_dir, 'include')
         if builtin_include in headers: headers.remove(builtin_include)
-        settings = sublime.load_settings(SETTINGS)
+        settings = sublime.load_settings('Clara.sublime-settings')
         username = getpass.getuser()
         hostname = socket.gethostname()
         key = '{}@{}'.format(username, hostname)
@@ -55,9 +52,9 @@ class ClaraWriteSystemHeadersCommand(sublime_plugin.ApplicationCommand):
             'system_headers': headers,
             'system_frameworks': frameworks}
         settings.set(key, header_dict)
-        sublime.save_settings(SETTINGS)
-        if sublime.ok_cancel_dialog(USER_MSG):
-            sublime.run_command('clara_open_settings')
+        sublime.save_settings('Clara.sublime-settings')
+        if sublime.ok_cancel_dialog('System headers have been saved in user settings. Do you want to view them now?'):
+            sublime.run_command('edit_settings', args={'base_file': '${packages}/Clara/Clara.sublime-settings'})
 
     def description(self):
-        return 'Writes system headers.'
+        return 'Write System Headers...'
