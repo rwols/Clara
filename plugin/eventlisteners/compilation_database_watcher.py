67ae933382b503ad40f36a5318c3182f422866b3
Raoul Wols <raoulwols@gmail.com>
2017-07-08 16:05:40 +0200

    Improve on_post_save from database watcher a bit

--- plugin/eventlisteners/compilation_database_watcher.py
+++ plugin/eventlisteners/compilation_database_watcher.py
@@ -1,14 +1,12 @@
 import sublime_plugin
 import Clara.Clara
+from .code_completer import CodeCompleter
 
 class CompilationDatabaseWatcher(sublime_plugin.EventListener, 
                                  Clara.Clara.CompilationDatabaseWatcher):
 
     def on_post_save(self, view):
-        listeners = sublime_plugin.view_event_listeners.get(view.id(), None)
-        if not listeners:
-            return
+        listeners = sublime_plugin.view_event_listeners.get(view.id(), [])
         for listener in listeners:
-            if hasattr(listener, "on_post_save"):
+            if isinstance(listener, CodeCompleter):
                 listener.on_post_save()
-                
